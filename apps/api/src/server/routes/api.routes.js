bb6fdf06f3ada2e6f4dd49e1e8d95ae4dc0558f0
polarisnexuslcc-byte <polarisnexuslcc@gmail.com>
2026-06-26 13:58:35 +0200

    Add documents module to apps/api

--- apps/api/src/server/routes/api.routes.js
+++ apps/api/src/server/routes/api.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { organizationsRouter } from '../../modules/organizations/organizations.routes.js';
 import { sourcesRouter } from '../../modules/sources/sources.routes.js';
+import { documentsRouter } from '../../modules/documents/documents.routes.js';
 
 export const apiRouter = Router();
 
@@ -13,3 +14,4 @@ apiRouter.get('/', (_req, res) => {
 
 apiRouter.use('/organizations', organizationsRouter);
 apiRouter.use('/sources', sourcesRouter);
+apiRouter.use('/documents', documentsRouter);
