96591ec9e852a15a4ce28c0319de4505cf4ecf39
polarisnexuslcc-byte <polarisnexuslcc@gmail.com>
2026-06-26 12:18:43 +0200

    Update index.js

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,5 +1,8 @@
 import { app } from './server/app.js';
 import { env } from './server/config/env.js';
+import { initializeDatabase } from './server/db/init.js';
+
+initializeDatabase();
 
 app.listen(env.PORT, () => {
   console.log(`[api] listening on http://localhost:${env.PORT}`);
