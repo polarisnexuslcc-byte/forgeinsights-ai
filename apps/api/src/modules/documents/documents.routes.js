8aa332a6788f5060a610736c7a288255246d85aa
polarisnexuslcc-byte <polarisnexuslcc@gmail.com>
2026-06-27 07:03:05 +0200

    Update documents.routes.js

--- apps/api/src/modules/documents/documents.routes.js
+++ apps/api/src/modules/documents/documents.routes.js
@@ -4,13 +4,22 @@ import {
   createDocumentVersionHandler,
   getDocumentHandler,
   listDocumentsHandler,
-  reindexDocumentHandler
+  reindexDocumentHandler,
+  uploadDocumentFileHandler
 } from './documents.controller.js';
+import { requireAuth } from '../../server/middleware/require-auth.js';
+import { uploadSingleDocument } from '../../server/storage/upload.js';
 
 export const documentsRouter = Router();
 
-documentsRouter.get('/', listDocumentsHandler);
-documentsRouter.get('/:id', getDocumentHandler);
-documentsRouter.post('/', createDocumentHandler);
-documentsRouter.post('/:id/versions', createDocumentVersionHandler);
-documentsRouter.post('/:id/reindex', reindexDocumentHandler);
+documentsRouter.get('/', requireAuth, listDocumentsHandler);
+documentsRouter.get('/:id', requireAuth, getDocumentHandler);
+documentsRouter.post('/', requireAuth, createDocumentHandler);
+documentsRouter.post('/:id/versions', requireAuth, createDocumentVersionHandler);
+documentsRouter.post('/:id/reindex', requireAuth, reindexDocumentHandler);
+documentsRouter.post(
+  '/:id/upload',
+  requireAuth,
+  uploadSingleDocument,
+  uploadDocumentFileHandler
+);
